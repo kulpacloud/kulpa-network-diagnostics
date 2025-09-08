787bbb733a3af740b435f53c10c1a997228f5086
Jorge Galrito <jorge.galrito@me.com>
2025-09-08 10:30:15 +0100

    Add browser capability tests

---
+++ src/ts/browser.ts
@@ -0,0 +1,27 @@
+﻿function testJavascript() {
+    document.getElementById('javascript-result')!.innerHTML = "&#x2705";
+}
+
+function testWebAssembly() {
+    try {
+        if (typeof WebAssembly === 'object' && typeof WebAssembly.instantiate === 'function') {
+            document.getElementById('wasm-result')!.innerHTML =  "&#x2705";
+        } else {
+            document.getElementById('wasm-result')!.innerHTML = "&#x274C";
+        }
+    }
+    catch {
+        document.getElementById('wasm-result')!.innerHTML = "&#x274C";
+    }
+}
+
+function testWebSockets() {
+    var webSocket = new WebSocket('wss://echo.websocket.org');
+    webSocket.onopen = (event) => {
+        document.getElementById('websocket-result')!.innerHTML = "&#x2705";
+        webSocket.close();
+    }
+    webSocket.onerror = (event) => {
+        document.getElementById('websocket-result')!.innerHTML = "&#x274C";
+    }
+}
